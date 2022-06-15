682f9db20b6ff4125ae4dfb4d4b6590a4615f7a3
yagusti26 <yasminagusti@gmail.com>
2022-06-14 22:25:30 -0300

    localStorage

--- script.js
+++ script.js
@@ -1,12 +1,56 @@
-
-
+const searchBar = document.getElementById("search");
+const deleteButton = document.getElementById("delete");
+const ul = document.getElementById("ul");
 
 form = document.querySelector('form')
+
+// obtener localStorage
+let recentSearches;
+
+if (localStorage.recentSearches && localStorage.recentSearches != "") {
+  recentSearches = JSON.parse(localStorage.recentSearches);
+} else {
+  recentSearches = [];
+}
+
+const makeListItem = (text, parent) => {
+  let listItem = document.createElement("li");
+  listItem.textContent = text;
+  listItem.className = "list-group-item";
+  parent.appendChild(listItem);
+};
+
+recentSearches.forEach(element => {
+  makeListItem(element, ul);
+});
+
+const isDuplicateValue = (arr, text) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == text) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+// form
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
     inpValue = e.target.querySelector('input').value
     fetchData4mAPI(inpValue)
+    if (
+      searchBar.value == "" ||
+      isDuplicateValue(recentSearches, searchBar.value)
+    ) {
+      return;
+    } else {
+      recentSearches.push(searchBar.value);
+      makeListItem(searchBar.value, ul);
+      localStorage.recentSearches = JSON.stringify(recentSearches);
+      searchBar.value = "";
+    }
 })
 
 async function fetchData4mAPI(inpVal) {
@@ -19,6 +63,21 @@ async function fetchData4mAPI(inpVal) {
     genrateHTML(datas.hits);
 }
 
+// borrar resultados
+deleteButton.addEventListener("click", () => {
+  localStorage.clear();
+  recentSearches = [];
+  searchBar.value = "";
+
+  let arr = document.querySelectorAll("li");
+
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].remove();
+  }
+});
+
+
+// Generar resultados
 function genrateHTML(results) {
     showINHTML = '';
     results.map(result => {
@@ -40,3 +99,7 @@ function genrateHTML(results) {
 
     })
 }
+
+
+
+
